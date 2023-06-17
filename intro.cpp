b88bb3230785587029d31f98fbdf02e5f184a496
shahzad ali <hirashahzad027@gmail.com>
2023-06-17 22:49:24 +0500

    first intro program

---
+++ intro.cpp
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout<<1;
+	cout<<endl;
+	cout<<3.01;
+	cout<<'A';
+	cout<<endl;
+	cout<<"hira";
+	getch();
+	return 0;
+}
+
+
+//name
+//qualification
+//city
+//country
